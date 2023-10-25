74479b44f36a756c84047ac888b531a1ce487a40
isamyakt <isamyakt17@gmail.com>
2023-10-25 13:45:06 +0530

    code excutes and displays 'hello, iced!'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("hello, world");
+use iced::{Sandbox, Settings, widget::text};
+
+fn main() -> iced::Result {
+    Editor::run(Settings::default())
+}
+
+
+struct Editor;
+
+#[derive(Debug)]
+enum Message {}
+
+impl Sandbox for Editor {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("iced editor")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {}
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        text("Hello, iced!").into()
+    }
 }
