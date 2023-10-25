ee44ac09a76c4472f11eed2d57852e0812fa331a
isamyakt <isamyakt17@gmail.com>
2023-10-25 14:15:57 +0530

    implemented dark theme and cursor indicator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use iced::{Sandbox, Settings, widget::{text_editor, container}};
+use iced::{Sandbox, Settings, Element, Theme, Length}; 
+use iced::widget::{column, text, text_editor, container, row, horizontal_space};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
@@ -19,7 +20,7 @@ impl Sandbox for Editor {
 
     fn new() -> Self {
         Self {
-            content: text_editor::Content::new(),
+            content: text_editor::Content::with(include_str!("main.rs")),
         }
     }
 
@@ -35,9 +36,24 @@ impl Sandbox for Editor {
         }
     }
 
-    fn view(&self) -> iced::Element<'_, Self::Message> {
+    fn view(&self) -> Element<'_, Self::Message> {
         let input = text_editor(&self.content).on_edit(Message::Edit);
 
-        container(input).padding(10).into()
+        let position = {
+            let (line, column) = self.content.cursor_position();
+
+            text(format!("{}:{}", line + 1, column + 1))
+        };
+
+        let status_bar = row![horizontal_space(Length::Fill), position];
+
+
+        container(column![input, status_bar])
+            .padding(10)
+            .into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Dark
     }
 }
